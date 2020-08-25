291991c6c87ebaac79c1d54059d2af590adc5174
Atle Wee Førre <ATFO@equinor.com>
2020-08-25 07:29:36 +0200

    Do not pass port as array

--- sacn-config.js
+++ sacn-config.js
@@ -1,17 +1,17 @@
 const e131 = require('e131')
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     function sACNConfigNode(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         var node = this
         node.hostOrUniverse = n.hostOrUniverse;
         node.port = n.port;
         node.flowContext = node.context().global
-        var hostOrUniverseHex = 
+        var hostOrUniverseHex =
             Number.isInteger(Number(node.hostOrUniverse))
                 ? Number(node.hostOrUniverse)
                 : node.hostOrUniverse
-        node.client = new e131.Client(hostOrUniverseHex,[node.port])
+        node.client = new e131.Client(hostOrUniverseHex, node.port)
 
         node.packet = node.flowContext.get("packet") || []
 
@@ -23,13 +23,13 @@ module.exports = function(RED) {
             return node.packet[universe].getSlotsData()
         }
 
-        node.getChannel = function (universe,channel) {
-            return node.getUniverse(universe)[channel-1]
+        node.getChannel = function (universe, channel) {
+            return node.getUniverse(universe)[channel - 1]
         }
 
         node.setChannel = function (universe, channel, value) {
             var slotsData = node.getUniverse(universe)
-            slotsData[channel-1] = value
+            slotsData[channel - 1] = value
             node.packet[universe].setSlotsData(slotsData)
         }
 
@@ -40,37 +40,36 @@ module.exports = function(RED) {
             }
             node.packet[universe].setSlotsData(slotsData)
         }
-        
+
         node.sendPacket = function (universe) {
             node.client.send(node.packet[universe])
         }
 
         node.transition = function (universe, channel, currentValue, targetValue, delay) {
-            if (    currentValue >= 0 
-                    && currentValue <= 255
-                    && targetValue >= 0
-                    && targetValue <= 255
-                    && currentValue != targetValue 
-                ) 
-                {
-                    var newValue = currentValue < targetValue
-                        ? currentValue + 1
-                        : currentValue - 1
-                    node.setChannel(universe, channel, newValue)
-                    node.sendPacket(universe)
-                    setTimeout(function () {node.transition(universe, channel, newValue, targetValue, delay)},delay)
-                }
+            if (currentValue >= 0
+                && currentValue <= 255
+                && targetValue >= 0
+                && targetValue <= 255
+                && currentValue != targetValue
+            ) {
+                var newValue = currentValue < targetValue
+                    ? currentValue + 1
+                    : currentValue - 1
+                node.setChannel(universe, channel, newValue)
+                node.sendPacket(universe)
+                setTimeout(function () { node.transition(universe, channel, newValue, targetValue, delay) }, delay)
+            }
         }
-        
+
         node.set = function (universe, channel, value, transition) {
             if (transition[0] == "rate" || transition[0] == "time") {
-                var oldValue = node.getChannel(universe,channel)
-                if (oldValue == value) {return;}
+                var oldValue = node.getChannel(universe, channel)
+                if (oldValue == value) { return; }
                 if (transition[0] == "rate") {
                     node.transition(universe, channel, oldValue, value, transition[1])
                 } else {
                     var steps = oldValue < value
-                        ? value - oldValue 
+                        ? value - oldValue
                         : oldValue - value
                     var delay = Math.round(transition[1] / steps)
                     node.transition(universe, channel, oldValue, value, delay)
@@ -85,15 +84,15 @@ module.exports = function(RED) {
             node.setChannels(parseInt(universe), values)
             node.sendPacket(parseInt(universe))
         }
-        
+
         node.savePacketToContext = function () {
             node.flowContext.set("packet", node.packet)
         }
-        
+
         node.on("close", function () {
             node.savePacketToContext()
             node.client = null
         })
     }
-    RED.nodes.registerType("sacn-config",sACNConfigNode);
+    RED.nodes.registerType("sacn-config", sACNConfigNode);
 }
