37d1294906462d1d45fc0ce4b1e18a8ae9983861
Atle Wee Førre <ATFO@equinor.com>
2020-08-25 08:05:45 +0200

    Accept messages without topic

--- sacn.js
+++ sacn.js
@@ -5,25 +5,26 @@ module.exports = function (RED) {
         node.server = RED.nodes.getNode(config.server)
 
         node.on("input", function (msg) {
-            if(node.server) {
-                var topic = msg.topic.split("/",2) || ""
+            if (node.server) {
+                var topic = msg.topic ? msg.topic.split("/", 2) : ""
                 var payload = {}
-                
+
                 payload.universe = msg.payload.universe || config.universe || parseInt(topic[0]) || 0
                 if (config.universe && !topic[1]) {
                     payload.channel = msg.payload.channel || config.channel || parseInt(topic[0]) || 0
                 } else {
                     payload.channel = msg.payload.channel || config.channel || parseInt(topic[1]) || 0
                 }
+
                 payload.transition = msg.payload.transition || config.transition || "instant"
                 if (payload.transition === "instant") {
-                    payload.transition = ["instant",0]
+                    payload.transition = ["instant", 0]
                 } else if (payload.transition === "rate") {
                     var rate = msg.payload.transitionRate || config.transitionRate || 50
-                    payload.transition = ["rate",rate]
+                    payload.transition = ["rate", rate]
                 } else {
                     var time = msg.payload.transitionTime || config.transitionTime || 1000
-                    payload.transition = ["time",time]
+                    payload.transition = ["time", time]
                 }
                 if (Array.isArray(msg.payload)) {
                     node.server.setArray(payload.universe, msg.payload)
